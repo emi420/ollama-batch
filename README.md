7d50a9bf2969097a9c4012599615d0a28f7b2645
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2024-10-13 16:25:27 -0300

    Python module, + argument for questionFirst

--- README.md
+++ README.md
@@ -1,17 +1,53 @@
 # Ollama Batch Text Processor
 
-This simple script run text LLM prompts over a list of texts and print the results as JSON. 
+This simple script run text LLM prompts over a list of texts
+and print the results as JSON.
 
-## Usage
+How it works? it contatenates your prompt with each text and call ollama over it.
+
+For example, if your prompt is:
+
+`Is this recipe a sweet dessert or salty food? answer only with a hashtag #sweet or #salty`
+
+And your texts are:
+
+`Fruit salad: apple, orange, pear, grape, strawberry, sugar`
+`Potatoes, mayonnaise, salt, black pepper, red onion, eggs`
+
+It will run:
+
+```
+Fruit salad: apple, orange, pear, grape, strawberry, sugar
+
+Is this recipe a sweet dessert or salty food? answer only with a hashtag #sweet or #salty
+```
+
+```
+Potatoes, mayonnaise, salt, black pepper, red onion, eggs
+
+Is this recipe a sweet dessert or salty food? answer only with a hashtag #sweet or #salty
+```
+
+
+## Quick start
+
+### Install
+
+Directly from the main branch:
+
+pip install git+https://github.com/emi420/ollama-batch
+
+### Usage
 
 ```sh
-python ollama-batch.py \
+ollama-batch \
     [--directory DIRECTORY] \
     [--file FILE] [--model MODEL] \
     [--prompt PROMPT] \
     [--prompt-file PROMPT_FILE] \
     [--json-property JSON_PROPERTY] \
-    [--json-append JSON_APPEND]
+    [--json-append JSON_APPEND] \
+    [--question-first]
 
 options:
   -h, --help
@@ -30,16 +66,18 @@ options:
             JSON property that you want to use
   --json-append JSON_APPEND
             Property that you want to append to the results
+  --question-first JSON_APPEND
+            First the question, then the prompt
 ```
 
 ### Examples
 
 ```bash
-python ollama-batch.py -d examples/recipes -p 'Is this recipe a sweet dessert or salty food?'
-python ollama-batch.py -d examples/recipes -p 'Is this recipe a sweet dessert or salty food?' --json-property=ingredients
-python ollama-batch.py -d examples/recipes -p 'Is this recipe a sweet dessert or salty food?' --json-property=title
-python ollama-batch.py -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt
-python ollama-batch.py -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt --json-append=title,url
+ollama-batch -d examples/recipes -p 'Is this recipe a sweet dessert or salty food?'
+ollama-batch -d examples/recipes -p 'Is this recipe a sweet dessert or salty food?' --json-property=ingredients
+ollama-batch -d examples/recipes -p 'Is this recipe a sweet dessert or salty food?' --json-property=title
+ollama-batch -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt
+ollama-batch -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt --json-append=title,url
 ```
 
 ## Quick start
@@ -47,13 +85,13 @@ python ollama-batch.py -f examples/recipes.json --prompt-file examples/sweet_or_
 ### Requirements
 
 1. Install [Ollama](https://ollama.com/download)
-2. Install the Python package `pip install ollama`
+2. Install the package `pip install ollama`
 3. Run Ollama `ollama serve`
 
 ### Run
 
 ```sh
-python ollama-batch.py -d examples/recipes \
+ollama-batch -d examples/recipes \
     -p "Is this recipe a sweet dessert or salty food" \
     > recipes_results.json
 ```
--- ollama_batch/ollama_batch.py
+++ ollama_batch/ollama_batch.py
@@ -12,7 +12,7 @@ import json
 import sys
 
 # Process files inside a directory
-def processDirectory(question, model, directory):
+def processDirectory(question, model, directory, questionFirst):
   files = sorted(os.listdir(directory))
   firstLine = True
   for filename in files:
@@ -22,12 +22,12 @@ def processDirectory(question, model, directory):
         sys.stdout.write(",\n")
       else:
         firstLine = False 
-      jsonObject = answerQuestion(file.read(), question, model=model)
+      jsonObject = answerQuestion(file.read(), question, model=model, questionFirst=questionFirst)
       sys.stdout.write(json.dumps(jsonObject, ensure_ascii=False))
       sys.stdout.flush()
 
 # Process a JSON file
-def processJSONFile(question, model, path, property, json_append):
+def processJSONFile(question, model, path, property, json_append, questionFirst):
     with open(path, 'r', encoding='utf-8') as file:
       jsonObject = json.loads(file.read())
       firstLine = True
@@ -36,7 +36,7 @@ def processJSONFile(question, model, path, property, json_append):
           sys.stdout.write(",\n")
         else:
           firstLine = False 
-        jsonObject = answerQuestion(item[property], question, model=model)
+        jsonObject = answerQuestion(item[property], question, model=model, questionFirst=questionFirst)
         if json_append:
            for prop in json_append:
               jsonObject[prop] = item[prop]
@@ -59,13 +59,17 @@ def answerQuestion(content, question, questionFirst = False, model = "llama3"):
 
 def main():
     args = argparse.ArgumentParser()
-    args.add_argument("--directory", "-d", help="Directory with files you want to process", type=str, default=None)
+    args.add_argument("--directory", "-d", help="Directory with files you want to process", type=str, 
+                      default=None)
     args.add_argument("--file", "-f", help="JSON file you want to process", type=str, default=None)
     args.add_argument("--model", "-m", help="Model you want to use", type=str, default="llama3")
     args.add_argument("--prompt", "-p", help="Prompt text", type=str, default=None)
     args.add_argument("--prompt-file", help="Text file with a prompt", type=str, default=None)
     args.add_argument("--json-property", help="JSON property that you want to use", type=str, default="content")
-    args.add_argument("--json-append", help="Property that you want to append to the results", type=str, default=None)
+    args.add_argument("--json-append", help="Property that you want to append to the results", type=str, 
+                      default=None)
+    args.add_argument("--question-first", help="First the question, then the prompt", default=False, 
+                      action='store_true')
     args = args.parse_args()
 
     prompt = None
@@ -79,12 +83,13 @@ def main():
     if prompt:
       print("[")
       if args.directory:
-          processDirectory(prompt, args.model, args.directory)
+          processDirectory(prompt, args.model, args.directory, args.question_first)
           print("\n]\n")
           return
       elif args.file:
           if (args.file[-4:] == "json"):
-            processJSONFile(prompt, args.model, args.file, args.json_property, args.json_append.split(",") if args.json_append else None)
+            processJSONFile(prompt, args.model, args.file, args.json_property, args.json_append.split(",") 
+                            if args.json_append else None, args.question_first)
             print("\n]\n")
             return
 
@@ -92,15 +97,18 @@ def main():
     print("")
     print("This script can run text prompts over a list texts and print the results as JSON.")
     print("")
-    print("python ollama-batch.py -h prints help")
+    print("ollama-batch -h prints help")
     print("")
-    print("Usage: python ollama-batch.py -d <directory> -p <prompt>")
-    print("       python ollama-batch.py -d examples/recipes -p 'Is this recipe a sweet dessert or salty food?' > recipes_result.json")
+    print("Usage: ollama-batch -d <directory> -p <prompt>")
+    print("       ollama-batch -d examples/recipes -p 'Is this recipe a sweet dessert or salty food?' > \
+          recipes_result.json")
     print("")
-    print("       python ollama-batch.py -f <json file> --prompt-file <prompt file>")
-    print("       python ollama-batch.py -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt")
-    print("       python ollama-batch.py -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt --json-property=ingredients")
-    print("       python ollama-batch.py -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt --json-append=title,url")
+    print("       ollama-batch -f <json file> --prompt-file <prompt file>")
+    print("       ollama-batch -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt")
+    print("       ollama-batch -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt \
+          --json-property=ingredients")
+    print("       ollama-batch -f examples/recipes.json --prompt-file examples/sweet_or_salty.txt \
+          --json-append=title,url")
 
 if __name__ == "__main__":
     main()
